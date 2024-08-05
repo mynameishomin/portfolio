89527866847afaa9686f5f208102d3c5da2ec83c
mynameishomin <mynameishomin@gmail.com>
2024-08-06 00:33:31 +0900

    next api cors 설정, 이 api nest로 바꿀 예정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    crossOrigin: 'anonymous',
     images: {
         domains: [
             "images.unsplash.com",
