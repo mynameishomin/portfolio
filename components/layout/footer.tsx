3ebe068addefd2b1817a7d3f3dd2936058d98839
mynameishomin <mynameishomin@gmail.com>
2024-09-04 10:18:27 +0900

    푸터 수정

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,7 @@ import Container from "./container";
 
 export default () => {
     return (
-        <footer className="h-10 shadow-inner hidden">
+        <footer className="h-20 shadow-inner">
             <Container className="flex items-center h-full">
                 <span className="block text-sm text-gray-400">
                     © 2024. Jung Homin. All rights reserved.
